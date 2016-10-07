1dd3d36154f55b7a072dfc1ea670d046972a6d3a
geniusofsimplicity <genius.of.simplicity@gmail.com>
2016-10-07 21:58:03 +0200

    task 5-6 done, not polished

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,13 +1,22 @@
-class Hangman
-	def initialize(filename)
-		@count = 6
-		@word = pick_word(filename).strip.scan(/./)
-		@guess = Array.new(@word.size, "_")	
-		@correct = Array.new(@word.size)
-	end	
+require "json"
 
-	def start
+class Hangman
+	def self.start		
+		answer = ""
+		loop do
+			puts "Do you want to start a new game (n) or load a saved one(l)?"
+			answer = gets.chomp
+			break if answer == "n" || answer == "l"
+		end
+		case answer
+		when "n" then return self.new("5desk.txt")
+		when "l" then return self.load
+		end
+	end
+
+	def play
 		until @count == 0
+			ask_to_save
 			display_status
 			letter = get_letter
 			update_result(letter)
@@ -20,6 +29,57 @@ class Hangman
 	end
 
 	private
+
+	def self.load
+		puts "Type in the name of the file."
+		filename = gets.chomp
+		data = self.from_json(filename)
+		self.new(nil, data["count"], data["word"], data["guess"])
+	end
+
+	def initialize(*args) #filename, count, word, guess
+		if args.size == 1
+			@count = 6
+			@word = pick_word(args[0]).strip.scan(/./)
+			@guess = Array.new(@word.size, "_")
+		else
+			@count = args[1]
+			@word = args[2]
+			@guess = args[3]
+		end
+	end	
+
+	def ask_to_save
+		answer = ""
+		loop do
+			puts "Do you want to save the game? (y/n)"
+			answer = gets.chomp
+			break if answer == "y" || answer == "n"
+		end
+		if answer == "y"
+			save
+		end
+	end
+
+	def save
+		puts "Please, type in save filemane: "		
+		filename = gets.chomp
+		File.open(filename, "w") do |file|
+			file.puts to_json
+		end
+	end
+
+	def to_json
+		JSON.dump({
+			count: @count,
+			word: @word,
+			guess: @guess			
+			})	
+	end
+
+	def self.from_json(filename)
+		JSON.load(File.read(filename))
+	end
 
 	def victory?
 		@word == @guess		
@@ -62,5 +122,5 @@ class Hangman
 	end
 end
 
-game = Hangman.new("5desk.txt")
-game.start
+game = Hangman.start
+game.play
